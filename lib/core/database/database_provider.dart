ccf18abcca0bfe2a57399f2d85e4d21ea434e67b
Aditya Tiwari <123093692+aaditya140103@users.noreply.github.com>
2026-09-12 02:54:51 +0530

    fix: infer Riverpod database ref type

--- lib/core/database/database_provider.dart
+++ lib/core/database/database_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kola/core/database/kola_database.dart';
 
-final kolaDatabaseProvider = Provider<KolaDatabase>((Ref ref) {
+final kolaDatabaseProvider = Provider<KolaDatabase>((ref) {
   final KolaDatabase database = KolaDatabase();
-  ref.onDispose(database.close);
+  ref.onDispose(() {
+    database.close();
+  });
   return database;
 });
