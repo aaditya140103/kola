3e1a55230abf28cf663430d1b6b0ed494c9ea093
Aditya Tiwari <123093692+aaditya140103@users.noreply.github.com>
2026-09-12 04:34:17 +0530

    feat: initialize pdfrx at app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kola/app/kola_app.dart';
+import 'package:pdfrx/pdfrx.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  pdfrxFlutterInitialize();
   runApp(const ProviderScope(child: KolaApp()));
 }
