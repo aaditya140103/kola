0708f929b5b6ee4e2f58a38933500ff4da6d5fd5
Aditya Tiwari <123093692+aaditya140103@users.noreply.github.com>
2026-09-12 03:09:12 +0530

    feat: add reactive app data providers

---
+++ lib/core/providers/app_data_providers.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kola/core/providers/repository_providers.dart';
+import 'package:kola/document/model/document_models.dart';
+import 'package:kola/features/annotations/domain/annotation_models.dart';
+import 'package:kola/features/progress/domain/reading_models.dart';
+
+final documentsProvider = StreamProvider<List<KolaDocument>>((ref) {
+  return ref.watch(documentRepositoryProvider).watchAll();
+});
+
+final readingListProvider = StreamProvider<List<PlannedReadingItem>>((ref) {
+  return ref.watch(readingRepositoryProvider).watchReadingList();
+});
+
+final readingGoalsProvider = StreamProvider<List<ReadingGoal>>((ref) {
+  return ref.watch(readingRepositoryProvider).watchGoals();
+});
+
+final allReadingSessionsProvider = StreamProvider<List<ReadingSession>>((ref) {
+  return ref.watch(readingRepositoryProvider).watchAllSessions();
+});
+
+final readingStateProvider = StreamProvider.family<ReadingState?, String>((
+  ref,
+  documentId,
+) {
+  return ref.watch(readingRepositoryProvider).watchState(documentId);
+});
+
+final readingCoverageProvider = StreamProvider.family<ReadingCoverage?, String>((
+  ref,
+  documentId,
+) {
+  return ref.watch(readingRepositoryProvider).watchCoverage(documentId);
+});
+
+final readingSessionsProvider = StreamProvider.family<List<ReadingSession>, String>((
+  ref,
+  documentId,
+) {
+  return ref.watch(readingRepositoryProvider).watchSessions(documentId);
+});
+
+final annotationsProvider = StreamProvider.family<List<Annotation>, String>((
+  ref,
+  documentId,
+) {
+  return ref.watch(annotationRepositoryProvider).watchForDocument(documentId);
+});
