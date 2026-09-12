09f715c6802e2dffced2a2cedf209518f96da3ba
Aditya Tiwari <123093692+aaditya140103@users.noreply.github.com>
2026-09-12 17:16:34 +0530

    Provide annotation management service

--- lib/core/providers/annotation_providers.dart
+++ lib/core/providers/annotation_providers.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kola/core/providers/repository_providers.dart';
 import 'package:kola/features/annotations/application/annotation_creation_service.dart';
+import 'package:kola/features/annotations/application/annotation_management_service.dart';
 
 final annotationCreationServiceProvider = Provider<AnnotationCreationService>((ref) {
   return AnnotationCreationService(ref.watch(annotationRepositoryProvider));
+});
+
+final annotationManagementServiceProvider = Provider<AnnotationManagementService>((ref) {
+  return AnnotationManagementService(ref.watch(annotationRepositoryProvider));
 });
