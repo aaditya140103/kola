980eb675810b6f2cbfba3885d62d3882ec8219f4
Aditya Tiwari <123093692+aaditya140103@users.noreply.github.com>
2026-09-12 19:45:35 +0530

    Expose recovered annotation geometry provider

--- lib/core/providers/annotation_providers.dart
+++ lib/core/providers/annotation_providers.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kola/core/providers/app_data_providers.dart';
 import 'package:kola/core/providers/document_engine_providers.dart';
 import 'package:kola/core/providers/repository_providers.dart';
 import 'package:kola/features/annotations/application/annotation_creation_service.dart';
+import 'package:kola/features/annotations/application/annotation_geometry_recovery_service.dart';
 import 'package:kola/features/annotations/application/annotation_management_service.dart';
 import 'package:kola/features/annotations/application/annotation_navigation_service.dart';
+import 'package:kola/features/annotations/domain/annotation_models.dart';
 
 final annotationCreationServiceProvider = Provider<AnnotationCreationService>((ref) {
   return AnnotationCreationService(ref.watch(annotationRepositoryProvider));
@@ -15,4 +18,25 @@ final annotationManagementServiceProvider = Provider<AnnotationManagementService
 
 final annotationNavigationServiceProvider = Provider<AnnotationNavigationService>((ref) {
   return AnnotationNavigationService(ref.watch(formatRegistryProvider));
+});
+
+final annotationGeometryRecoveryServiceProvider =
+    Provider<AnnotationGeometryRecoveryService>((ref) {
+      return AnnotationGeometryRecoveryService(ref.watch(formatRegistryProvider));
+    });
+
+final recoveredAnnotationGeometryProvider = FutureProvider.family<
+  Map<String, List<Map<String, Object?>>>,
+  String
+>((ref, documentId) async {
+  final document = await ref.watch(documentProvider(documentId).future);
+  if (document == null) {
+    return const <String, List<Map<String, Object?>>>{};
+  }
+  final List<Annotation> annotations = await ref.watch(
+    annotationsProvider(documentId).future,
+  );
+  return ref
+      .watch(annotationGeometryRecoveryServiceProvider)
+      .recover(document, annotations);
 });
